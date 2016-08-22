7c8df1b9bfc1eb512764d2b913b54edb8abbd9b7
素人派 <zxjlwt@126.com>
2016-08-22 07:25:41 +0000

    no commit message

---
+++ src/org/suren/autotest/test/page/SuRenPiHomePage.java
@@ -0,0 +1,75 @@
+/**
+ * http://surenpi.com
+ */
+package org.suren.autotest.test.page;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.suren.autotest.web.framework.core.ui.Button;
+import org.suren.autotest.web.framework.core.ui.CheckBoxGroup;
+import org.suren.autotest.web.framework.core.ui.Text;
+import org.suren.autotest.web.framework.page.Page;
+
+/**
+ * 类上必须添加注解@Componentn</br>
+ * 属性上必须添加注解@Autowired以及对应的getter和setter方法
+ * @author suren
+ * @date Jul 23, 2016 4:41:16 PM
+ */
+@Component
+public class SuRenPiHomePage extends Page
+{
+	/** 首页的搜索文本框 */
+	@Autowired
+	private Text searchText;
+	@Autowired
+	private Button closeBut;
+	@Autowired
+	private CheckBoxGroup checkBoxGroup;
+	@Autowired
+	private Text chargeText;
+	@Autowired
+	private Button chargeBut;
+
+	public Text getSearchText()
+	{
+		return searchText;
+	}
+
+	public void setSearchText(Text searchText)
+	{
+		this.searchText = searchText;
+	}
+
+	public Button getCloseBut() {
+		return closeBut;
+	}
+
+	public void setCloseBut(Button closeBut) {
+		this.closeBut = closeBut;
+	}
+
+	public CheckBoxGroup getCheckBoxGroup() {
+		return checkBoxGroup;
+	}
+
+	public void setCheckBoxGroup(CheckBoxGroup checkBoxGroup) {
+		this.checkBoxGroup = checkBoxGroup;
+	}
+
+	public Button getChargeBut() {
+		return chargeBut;
+	}
+
+	public void setChargeBut(Button chargeBut) {
+		this.chargeBut = chargeBut;
+	}
+
+	public Text getChargeText() {
+		return chargeText;
+	}
+
+	public void setChargeText(Text chargeText) {
+		this.chargeText = chargeText;
+	}
+}
